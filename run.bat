eeb0741798b2bd1767c00224d936a847ccbcea31
Volodymyr <vlsv00@gmail.com>
2026-05-11 20:43:56 +0300

    Refactor run.bat for improved execution and add run.bat.example for reference

--- run.bat
+++ run.bat
@@ -1,11 +1,2 @@
-@echo off
-REM Активація venv та запуск сервісу
-if not exist venv\Scripts\activate.bat (
-    echo Віртуальне середовище не знайдено. Запустіть setup.bat
-    exit /b 1
-)
-call venv\Scripts\activate.bat
-@REM python -m uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
-python -m uvicorn app.main:app --env-file .env --host 0.0.0.0 --port 8000 --reload
-
-
+cd /d "%~dp0"
+"%~dp0venv\Scripts\Python.exe" -m uvicorn "app.main:app" --env-file "%~dp0.env" --host 0.0.0.0 --port 8000 --reload
